72ecbcb665a269cd0f60a0cd71ab84b06aa5d95e
Darren Torchia <darren.torchia@aerian.com>
2019-06-17 11:56:42 +0100

    pass through id for get req

--- src/components/ApplicationPage/ApplicationPage.tsx
+++ src/components/ApplicationPage/ApplicationPage.tsx
@@ -7,12 +7,16 @@ import { getFundingApplication } from "../../graphql/queries";
 import { useQuery } from "react-apollo-hooks";
 import gql from "graphql-tag";
 
-interface Props extends RouteComponentProps {}
+interface Props extends RouteComponentProps {
+    id?: string;
+}
 
 const GET_APPLICATION = gql(getFundingApplication);
 
-export const ApplicationPage: FC<Props> = props => {
-    const { data } = useQuery<GetFundingApplicationQuery>(GET_APPLICATION);
+export const ApplicationPage: FC<Props> = ({ id }) => {
+    const { data } = useQuery<GetFundingApplicationQuery>(GET_APPLICATION, {
+        variables: { id }
+    });
 
     return <Application application={data} />;
 };
